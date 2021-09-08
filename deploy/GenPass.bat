ff940615f238959e7d433a11f2a8722325e90ba7
贰拾壹 <30471543+er10yi@users.noreply.github.com>
2021-09-08 17:33:51 +0800

    Delete deploy directory

--- deploy/GenPass.bat
+++
@@ -1,3 +0,0 @@
-java -cp GenPass.jar com.eveino.GenPass
-@echo off
-pause
--- deploy/defaultConf/initMySQL.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# 初始化数据库
-# @author 贰拾壹
-# https://github.com/er10yi
-
-echo  "*****info*****   docker MySQL导入数据"
-mysql -uroot -p8TAQRc9EOkV607qm -e"CREATE DATABASE magicude DEFAULT CHARACTER SET utf8" >/dev/null 2>&1
-mysql -uroot -p8TAQRc9EOkV607qm -Dmagicude -e"SOURCE /usr/local/magicude.sql" >/dev/null 2>&1
